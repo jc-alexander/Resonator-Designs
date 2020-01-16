71c8a9ede6efae6bfb7cb4626121c9a02268f102
Joe Alexander <ucapjal@ucl.ac.uk>
2020-01-16 14:31:32 +0000

    add variable to gap in omega resonator

--- res_shapes.py
+++ res_shapes.py
@@ -634,10 +634,10 @@ def interdigital_capacitor(y0,l_arm,w_cap,gap,n):
         y0 = y0 + (2*w_cap + 2*gap)
     return capacitor
 
-def solidarc(x0,y0,r,w_ind,gap,w_cap,npoints=51):
+def solidarc(x0,y0,r,w_ind,o_gap,npoints=51):
     n=npoints
 
-    theta = (gap+w_cap)/r # Angle swept out by gap in omega
+    theta = (o_gap)/r # Angle swept out by gap in omega
     t0 = -np.pi/2 + theta/2 # In radians
     tf = (3/2)*np.pi - theta/2
 
@@ -646,16 +646,16 @@ def solidarc(x0,y0,r,w_ind,gap,w_cap,npoints=51):
 
     return [inner+outer]
 
-def interdig_optires(l_arm,w_cap,gap,n,r,w_ind):
+def interdig_optires(l_arm,w_cap,gap,n,r,w_ind,o_gap):
 
     design = []
 
-    design += [rect(l_arm/2,w_cap,0,0)]
-    design += [rect(l_arm/2,w_cap,w_cap+gap+l_arm/2,0)]
-    design += [rect(w_cap,2*w_cap,l_arm/2-w_cap,w_cap)]
-    design += [rect(w_cap,2*w_cap,l_arm/2+w_cap+gap,w_cap)]
-    design += [rect(l_arm/2,w_cap,0,3*w_cap)]
-    design += [rect(l_arm/2,w_cap,l_arm/2+w_cap+gap,3*w_cap)]
+    design += [rect((l_arm+gap+w_cap)/2 - gap/2,w_cap,0,0)]
+    design += [rect((l_arm+gap+w_cap)/2 - gap/2,w_cap,(l_arm+gap+w_cap)/2 + gap/2,0)]
+    design += [rect(w_cap,2*w_cap,(l_arm+gap+w_cap)/2 - gap/2-w_cap,w_cap)]
+    design += [rect(w_cap,2*w_cap,(l_arm+gap+w_cap)/2 + gap/2,w_cap)]
+    design += [rect((l_arm+gap+w_cap)/2 - gap/2,w_cap,0,3*w_cap)]
+    design += [rect((l_arm+gap+w_cap)/2 - gap/2,w_cap,(l_arm+gap+w_cap)/2 + gap/2,3*w_cap)]
     design += [rect(w_cap,gap,0,4*w_cap)]
     design += [rect(w_cap,gap,l_arm+gap,4*w_cap)]
 
@@ -663,15 +663,15 @@ def interdig_optires(l_arm,w_cap,gap,n,r,w_ind):
 
     design += interdigital_capacitor(y0,l_arm,w_cap,gap,n)
 
-    theta = (gap+w_cap)/r # Angle swept out by gap in omega
+    theta = o_gap/r # Angle swept out by gap in omega
     t0 = -np.pi/2 + theta/2 # In radians
     tf = (3/2)*np.pi - theta/2
 
-    design += [rect(l_arm/2,w_cap,0,y0+n*(2*w_cap+2*gap))]
-    design += [rect(l_arm/2,w_cap,w_cap+gap+l_arm/2,y0+n*(2*w_cap+2*gap))]
-    design += [rect(w_ind,r*np.sin(tf)+r+w_ind,l_arm/2-w_ind,y0+n*(2*w_cap+2*gap)+w_cap)]
-    design += [rect(w_ind,r*np.sin(tf)+r+w_ind,l_arm/2+w_cap+gap,y0+n*(2*w_cap+2*gap)+w_cap)]
+    design += [rect((l_arm+gap+w_cap)/2 - o_gap/2,w_cap,0,y0+n*(2*w_cap+2*gap))]
+    design += [rect((l_arm+gap+w_cap)/2 - o_gap/2,w_cap,(l_arm+gap+w_cap)/2 + o_gap/2,y0+n*(2*w_cap+2*gap))]
+    design += [rect(w_ind,r*np.sin(tf)+r+w_ind,(l_arm+gap+w_cap)/2 - o_gap/2 - w_ind,y0+n*(2*w_cap+2*gap)+w_cap)]
+    design += [rect(w_ind,r*np.sin(tf)+r+w_ind,(l_arm+gap+w_cap)/2 + o_gap/2,y0+n*(2*w_cap+2*gap)+w_cap)]
 
-    design += solidarc((l_arm+w_cap+gap)/2,y0 + n*(2*w_cap+2*gap)+w_cap+r+w_ind,r,w_ind,gap,w_cap)
+    design += solidarc((l_arm+w_cap+gap)/2,y0 + n*(2*w_cap+2*gap)+w_cap+r+w_ind,r,w_ind,o_gap)
 
     return design
