4f5731365b39b8e12d6c30f49124539b17c694e8
Joe Alexander <ucapjal@ucl.ac.uk>
2020-01-22 11:59:20 +0000

    updates

--- OptiRes_CST.py
+++ OptiRes_CST.py
@@ -6,11 +6,12 @@ import numpy as np
 
 #parameters (um)
 l_arm = 100
-w_cap = 10
+w_cap = 25
 gap = 10
-n = 2
-r = 20
-w_ind = 5
+n = 3
+r = 10
+w_ind = 1
+o_gap = 0.5
 
 #setup the folder and gds 'cell'
 
@@ -29,8 +30,10 @@ bckgrnd += [[(0,170), (100,170), (100, 1170), (0,1170)]]
 bckgrnd += [[(900,170), (900,1170), (1000, 1170), (1000,170)]]
 bckgrnd += [[(0,1170), (1000,1170), (1000, 1370), (0,1370)]]
 
+#for i in bckgrnd:
+#    poly_cell.add(gdspy.Polygon(i, 0))
 
-for j in np.linspace(5,20,1):
+for r in np.linspace(5,25,5):
     #clear
     poly_cell.polygons=[]
     #sort out the ground plane, feedline etc
@@ -38,19 +41,22 @@ for j in np.linspace(5,20,1):
         poly_cell.add(gdspy.Polygon(i, 0))
 
     #add the resonator
-    res = rs.interdig_optires(l_arm, w_cap, gap, n, r, w_ind)
+    res = rs.interdig_optires(l_arm, w_cap, gap, n, r, w_ind,o_gap)
     min_yr = min([min(i) for i in [[i[q][1] for q in range(len(i))] for i in res]])
     max_yr = max([max(i) for i in [[i[q][1] for q in range(len(i))] for i in res]])
-    res = [rs.move(i, 450,  220 - min_yr) for i in res]
+    #res = [rs.move(i, 450,  220 - min_yr) for i in res]
+    res = [rs.move(i, 150 + n*150,  220 - min_yr) for i in res]
 
     for i in res:
         poly_cell.add(gdspy.Polygon(i, 1))
 
-    y0 = max_yr - w_ind - r
-    cover = rs.solidarc((l_arm+gap+w_cap)/2,y0,r,w_ind,gap,w_cap)
-    cover = [rs.move(i, 450,  220 - min_yr) for i in cover]
+    #y0 = max_yr - w_ind - r
+    #cover = rs.solidarc((l_arm+gap+w_cap)/2,y0,r,w_ind,o_gap)
+    #cover = [rs.move(i, 450,  220 - min_yr) for i in cover]
+    #cover = [rs.move(i,150 + n*150, 220 - min_yr) for i in cover]
 
-    for i in cover:
-        poly_cell.add(gdspy.Polygon(i, 2))
-    #gdspy.write_gds('multidig_1_turn_{}um_gap.gds'.format(j,50), unit=1.0e-6, precision=1.0e-9)
-    gdspy.LayoutViewer()
+    #for i in cover:
+#            poly_cell.add(gdspy.Polygon(i, 2))
+    gdspy.write_gds('changing_ind_r_{}um.gds'.format(r), unit=1.0e-6, precision=1.0e-9)
+
+gdspy.LayoutViewer()
